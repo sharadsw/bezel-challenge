a2195065f8aecf6591c008a0b5aafd1e47a82668
Sharad <16229739+sharadsw@users.noreply.github.com>
2023-05-03 22:51:57 -0400

    Add hr and button components

---
+++ src/components/Button.tsx
@@ -0,0 +1,39 @@
+import { FunctionComponent } from "react";
+
+type ButtonProps = {
+  className?: string;
+  title: string;
+  onClick: () => void;
+};
+
+const DEFAULT_CLASSES = ` 
+p-4 
+font-semibold 
+leading-normal 
+border-none 
+text-center 
+no-underline 
+align-middle 
+cursor-pointer 
+select-none 
+text-base 
+rounded-3xl 
+transition-colors 
+ease-in-out 
+delay-15`;
+
+export const ButtonPrimary:FunctionComponent<ButtonProps> = ({ className, title, onClick }) => {
+  return (
+    <button className={`bg-green-600 hover:bg-green-700 text-white ${DEFAULT_CLASSES} ${className}`} onClick={onClick}>
+      {title}
+    </button>
+  );
+};
+
+export const ButtonClear:FunctionComponent<ButtonProps> = ({ className, title, onClick }) => {
+    return (
+      <button className={`bg-transparent hover:bg-beige-200 text-green-500 ${DEFAULT_CLASSES} ${className}`} onClick={onClick}>
+        {title}
+      </button>
+    );
+  };
---
+++ src/components/HR.tsx
@@ -0,0 +1,9 @@
+import { FunctionComponent } from "react";
+
+export const HR: FunctionComponent = () => {
+  return (
+    <>
+      <hr className="bg-gray-dark-200 h-0.5 border-none w-98p" />
+    </>
+  );
+};
